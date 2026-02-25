9292c924d9ffb83a7c8a5216a874e72d0d394c70
armenmkrtchyan <arm.mkrtchyan07@gmail.com>
2026-02-25 14:20:46 +0400

    First commit

---
+++ src/components/RemoteButton.tsx
@@ -0,0 +1,19 @@
+const RemoteButton = () => {
+  return (
+    <button
+      className="
+        px-4 py-2
+        rounded-lg
+        bg-indigo-600
+        text-white
+        font-medium
+        hover:bg-indigo-700
+        transition-colors
+      "
+    >
+      Button from Remote App
+    </button>
+  )
+}
+
+export default RemoteButton
